35defe03820d1a443bd80e99ca7af5a958865b3f
“Venu” <Venuchalla1993@gmail.com>
2025-07-12 00:51:29 -0500

    updated

--- src/main/java/com/example/parent/ParentApplication.java
+++ src/main/java/com/example/parent/ParentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParentApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ParentApplication.class, args);
 	}
 
