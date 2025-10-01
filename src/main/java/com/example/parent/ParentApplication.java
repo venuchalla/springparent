0ac5199a01d546bfd0aeb8c78ce6a106e20ce87e
“Venu” <Venuchalla1993@gmail.com>
2025-10-01 13:39:33 -0500

    updated git work flow

--- src/main/java/com/example/parent/ParentApplication.java
+++ src/main/java/com/example/parent/ParentApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ParentApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(ParentApplication.class, args);
 	}
 
