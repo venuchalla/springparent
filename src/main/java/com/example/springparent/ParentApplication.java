6dd70b3586ce4528b48daee8f1c49cedc50f8ff7
“Venu” <Venuchalla1993@gmail.com>
2026-01-04 20:10:28 -0600

    updated files

--- src/main/java/com/example/springparent/ParentApplication.java
+++ src/main/java/com/example/springparent/ParentApplication.java
@@ -1,4 +1,4 @@
-package com.example.parent;
+package com.example.springparent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
