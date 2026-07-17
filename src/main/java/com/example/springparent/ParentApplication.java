02c25dd5eed27bac7252885b0b686ca9807b81fb
“Venu” <Venuchalla1993@gmail.com>
2026-07-16 19:58:56 -0500

    updated pom

--- src/main/java/com/example/springparent/ParentApplication.java
+++ src/main/java/com/example/springparent/ParentApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ParentApplication {
 
+public class ParentApplication {
+private Logger logger = LoggerFactory.getLogger(ParentApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(ParentApplication.class, args);
+		logger.info("Parent application started");
 	}
 
 }
